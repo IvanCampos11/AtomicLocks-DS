13df1ef0951f20b16e31f2b9957249f50bb12778
Ivan Campos <39417756+IvanCampos11@users.noreply.github.com>
2021-05-25 01:19:12 -0400

    Update viz.py

--- Data Science/app/viz.py
+++ Data Science/app/viz.py
@@ -3,3 +3,10 @@
 from fastapi import APIRouter
 
 router = APIRouter()
+
+
+@router.get('/viz')
+async def predict():
+    """returns 200000. Legit nothing else."""
+    y_pred = 'E'
+    return {'here your viz': y_pred}
